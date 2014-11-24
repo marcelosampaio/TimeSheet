d6c10047ff0fbd644444949aad79a147a4bfd917
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-11-23 23:15:02 -0200

    added tableView to main View

--- TimeSheet/ViewController.m
+++ TimeSheet/ViewController.m
@@ -19,7 +19,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    
+    
+    // Configure UI
+    [self configureUI];
+    
+    // Load Data
     self.source=[[NSMutableArray alloc]init];
+    self.source=[self loadSource];
     
     NSLog(@"initial commit");
 }
@@ -30,15 +37,31 @@
 }
 
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+-(UITableViewCell *)tableView:(UITableView *)a_tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    static NSString *CellIdentifier = @"Cell";
     
-    return [[UITableViewCell alloc]init];
+    UITableViewCell *cell = [a_tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    if (cell==nil) {
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:CellIdentifier];
+    }
+    cell.textLabel.text=[self.source objectAtIndex:indexPath.row];
+    
+    return cell;
     
 }
 
+#pragma mark - UI Configuration
+-(void)configureUI {
+    NSLog(@"configureUI");
+    self.navigationItem.title=@"Nov/2014";
+    
+}
 
-
+#pragma mark - Working Methods
+-(NSMutableArray *)loadSource {
+    return [[NSMutableArray alloc]initWithObjects:@"first",@"second",@"third",@"fourth", nil];
+}
 
 
 @end
